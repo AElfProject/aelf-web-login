99dcc637363e1043d9a98611024fa0f3a4002cbd
石头 <peter.zhang@aelf.io>
2023-06-15 18:14:57 +0800

    feat(remove): remove code note

--- packages/login/src/components/CofirmLogoutDialog/ConfirmLogoutDialog.tsx
+++ packages/login/src/components/CofirmLogoutDialog/ConfirmLogoutDialog.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: 石头 peter.zhang@aelf.io
- * @Date: 2023-06-15 10:23:58
- * @LastEditors: 石头 peter.zhang@aelf.io
- * @LastEditTime: 2023-06-15 18:07:10
- * @FilePath: /aelf-web-login/packages/login/src/components/CofirmLogoutDialog/ConfirmLogoutDialog.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React, { useMemo } from 'react';
 import { Modal, Button, Row } from 'antd';
 import isMobile from '../../utils/isMobile';
