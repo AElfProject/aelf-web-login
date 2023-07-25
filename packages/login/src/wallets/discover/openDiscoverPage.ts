fa7dafd47b04cfd9d582572f5f4891bd8148d2e3
holymingo <holymingo@aelf.io>
2023-07-25 15:39:37 +0800

    feat: release 1.0.3-alpha

--- packages/login/src/wallets/discover/openDiscoverPage.ts
+++ packages/login/src/wallets/discover/openDiscoverPage.ts
@@ -2,7 +2,7 @@ import { scheme } from '@portkey/utils';
 
 export default function openPageInDiscover(url?: string) {
   window.location.href = scheme.formatScheme({
-    action: 'login',
+    action: 'linkDapp',
     domain: window.location.host,
     custom: {
       url: url || window.location.href,
