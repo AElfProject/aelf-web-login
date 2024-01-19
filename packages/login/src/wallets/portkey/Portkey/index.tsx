9779ffde03382b1521a6ab5842c5951550c22377
potter <potter.sun@aelf.io>
2024-01-19 23:41:14 +0800

    perf: ⚡️ switch version no close modal

--- packages/login/src/wallets/portkey/Portkey/index.tsx
+++ packages/login/src/wallets/portkey/Portkey/index.tsx
@@ -173,7 +173,7 @@ export default function Portkey({
     <SignInComponent
       defaultChainId={chainId as any}
       ref={signInRef}
-      uiType="Modal"
+      uiType="Full"
       design={portkeyOpts.design}
       isShowScan
       extraElementList={extraElement}
