2325b0904102178bdabd10dc0ed29f09a8530160
potter <potter.sun@aelf.io>
2024-02-06 19:00:16 +0800

    fix: 🐛 discover connect

--- packages/login/src/wallets/discover/detectProvider.ts
+++ packages/login/src/wallets/discover/detectProvider.ts
@@ -2,8 +2,8 @@ import detectProvider from '@portkey/detect-provider';
 import { IPortkeyProvider } from '@portkey/provider-types';
 import { WEB_LOGIN_VERSION } from '../../constants';
 
-export default async function detectDiscoverProvider(): Promise<IPortkeyProvider | null> {
-  const version = localStorage.getItem(WEB_LOGIN_VERSION);
+export default async function detectDiscoverProvider(version?: string | null): Promise<IPortkeyProvider | null> {
+  version = version || localStorage.getItem(WEB_LOGIN_VERSION);
   let detectProviderFunc = detectProvider;
   if (typeof detectProvider !== 'function') {
     const detectProviderModule = detectProvider as any;
