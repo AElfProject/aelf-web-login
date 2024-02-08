d4c5e195fd578cac75da5372cf9404b19f8df0a9
AbigailDeng <Abigail.deng@ienyan.com>
2024-02-08 13:55:28 +0800

    feat: openPageInDiscover

--- packages/login/src/wallets/discover/openDiscoverPage.ts
+++ packages/login/src/wallets/discover/openDiscoverPage.ts
@@ -10,7 +10,20 @@ export default function openPageInDiscover(url?: string, version?: 'v1') {
         url: url || window.location.href,
       },
     });
+    const downloadUrl = 'https://portkey.finance/';
+    setTimeout(() => {
+      const hidden =
+        window.document.hidden ||
+        (window.document as any).mozHidden ||
+        (window.document as any).msHidden ||
+        (window.document as any).webkitHidden;
+      if (typeof hidden !== 'undefined' && hidden === true) {
+        return;
+      }
+      window.location.href = downloadUrl;
+    }, 2000);
   } catch (error) {
+    // can‘t open app cannot go into this logic
     console.error(error);
     window.open('https://portkey.finance', '_blank');
   }
