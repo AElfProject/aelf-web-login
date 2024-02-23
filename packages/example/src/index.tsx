3b1507d8cfabecb631c5813409db5e1fde7770db
AbigailDeng <Abigail.deng@ienyan.com>
2024-02-23 10:00:49 +0800

    feat: style file in the login

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'antd/dist/antd.css';
-import '@portkey/did-ui-react/dist/assets/index.css';
-import '@portkey-v1/did-ui-react/dist/assets/index.css';
+// import '@portkey/did-ui-react/dist/assets/index.css';
+// import '@portkey-v1/did-ui-react/dist/assets/index.css';
 import 'aelf-web-login/dist/assets/index.css';
 import './index.css';
 import './config';
