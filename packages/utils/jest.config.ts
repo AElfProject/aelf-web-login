7c8802d538318d0646ef238687020750af7a74d1
aelf-lxy <xiyang.liu@aelf.io>
2024-07-29 16:44:58 +0800

    feat: test badge

--- packages/utils/jest.config.ts
+++ packages/utils/jest.config.ts
@@ -93,7 +93,16 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '.',
+        outputName: 'jest-report.xml',
+      },
+    ],
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
