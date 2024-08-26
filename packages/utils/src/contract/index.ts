74a7f5a263d8707ff7df8d6edca43370f7bafa72
aelf-lxy <xiyang.liu@aelf.io>
2024-08-26 16:02:02 +0800

    feat: add did-ui-react as peerDependencies

--- packages/utils/src/contract/index.ts
+++ packages/utils/src/contract/index.ts
@@ -2,3 +2,4 @@ export * from './useCheckAllowanceAndApprove';
 export * from './useGetBalance';
 export * from './getRawTransaction';
 export * from './getTxResultRetry';
+export * from './callViewMethod';
